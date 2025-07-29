d78c08a4b143f769b3eac5585ac1d4c174f5ee6f
Kshruti-01 <shrutiraj1206@gmail.com>
2025-07-30 01:29:19 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+#setup the project as local package
+from setuptools import setup, find_packages
+setup(
+    name="HR_Chatbot_GENAI",
+    version="0.1",
+    author="Shruti",
+    author_email="shrutiraj1206@gmail.com",
+    packages=find_packages(),
+    install_requires=[],  # Add your package dependencies here
+)
